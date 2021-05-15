c1c2c732e41c699ed8cbb2a96517bbf7ff92855d
gortiz <ggortiz09@gmail.com>
2021-05-15 13:12:07 -0300

    first commit

---
+++ src/main/java/com/gortiz/rest/restajax/RestAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.gortiz.rest.restajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAjaxApplication.class, args);
+	}
+
+}
