57cc7363ced3b62c560274f549149b41d6293eb1
gortiz <ggortiz09@gmail.com>
2021-05-16 23:49:50 -0300

    test controller

--- src/main/java/com/gortiz/rest/restajax/controller/Controller.java
+++ src/main/java/com/gortiz/rest/restajax/controller/Controller.java
@@ -1,8 +1,14 @@
 package com.gortiz.rest.restajax.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
+	@GetMapping("/hola")
+	String saudos() {
+		return "view-person";
+	}
 
 }
