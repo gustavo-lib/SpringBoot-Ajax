af72bf17401fd9aaf5730ba275bfe0374aeca961
gortiz <ggortiz09@gmail.com>
2021-05-19 22:11:00 -0300

    service

---
+++ src/main/java/com/gortiz/rest/restajax/service/PersonService.java
@@ -0,0 +1,9 @@
+package com.gortiz.rest.restajax.service;
+
+import com.gortiz.rest.restajax.entity.Person;
+
+public interface PersonService {
+	
+	Person save(Person p);
+
+}
---
+++ src/main/java/com/gortiz/rest/restajax/service/PersonServiceImpl.java
@@ -0,0 +1,16 @@
+package com.gortiz.rest.restajax.service;
+
+import org.springframework.stereotype.Service;
+
+import com.gortiz.rest.restajax.entity.Person;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+	@Override
+	public Person save(Person p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
