faaf27ceffe9ef0353f252c5ab31d898bc132ed9
gortiz <ggortiz09@gmail.com>
2021-05-26 22:58:05 -0300

    method getPersons and save , whit Personrespository

--- src/main/java/com/gortiz/rest/restajax/service/PersonService.java
+++ src/main/java/com/gortiz/rest/restajax/service/PersonService.java
@@ -1,9 +1,13 @@
 package com.gortiz.rest.restajax.service;
 
+import java.util.List;
+
 import com.gortiz.rest.restajax.entity.Person;
 
 public interface PersonService {
 	
 	Person save(Person p);
+	
+	List<Person> getPersons();
 
 }
--- src/main/java/com/gortiz/rest/restajax/service/PersonServiceImpl.java
+++ src/main/java/com/gortiz/rest/restajax/service/PersonServiceImpl.java
@@ -1,16 +1,31 @@
 package com.gortiz.rest.restajax.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gortiz.rest.restajax.entity.Person;
+import com.gortiz.rest.restajax.respository.PersonRepository;
 
 @Service
 public class PersonServiceImpl implements PersonService {
 
+	@Autowired
+	PersonRepository personRepository;
+	
 	@Override
 	public Person save(Person p) {
 		// TODO Auto-generated method stub
-		return null;
+		return personRepository.save(p);
 	}
+
+	@Override
+	public List<Person> getPersons() {
+		// TODO Auto-generated method stub
+		return personRepository.findAll();
+	}
+	
+	
 
 }
